fa0d1d0b94226a5c8a2c6ebdf3c11458d7eb3a50
mxnc <thibaultmaxence44@gmail.com>
2024-02-07 01:53:12 +0100

    update readme

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" task 7. Complex types - string and int/float to tuple """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ type-annotated function to_kv    """
+    return sum(input_list)
