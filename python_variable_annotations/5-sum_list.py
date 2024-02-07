eed8625594fd859392759af3dd943247b2ac5df0
mxnc <thibaultmaxence44@gmail.com>
2024-02-07 01:35:42 +0100

    task 5. Complex types - list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" task 5. Complex types - list of floats """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ type-annotated function sum_list """
+    return sum(input_list)
