805292d48acb070ff707c2dc05ebf408528944dd
mxnc <thibaultmaxence44@gmail.com>
2024-02-08 01:04:34 +0100

    fixing and initializing

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ return the list of all the delays """
     delays = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    tasks = [wait_random(max_delay) for i in range(n)]
 
     for task in asyncio.as_completed(tasks):
         result = await task
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ return the list of all the delays """
     delays = []
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for i in range(n)]
 
     for task in asyncio.as_completed(tasks):
         result = await task
