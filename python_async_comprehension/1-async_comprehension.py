52737c12f47c9eac17e986e71d01b1386df0708c
mxnc <thibaultmaxence44@gmail.com>
2024-02-08 01:31:58 +0100

    task 1. Async Comprehensions fixed

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,4 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """ async_comprehension coroutine """
     return [n async for n in async_generator()]
