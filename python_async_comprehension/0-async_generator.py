bf4ef009bf7a3206a5360b72c0cfff30c4730906
mxnc <thibaultmaxence44@gmail.com>
2024-02-08 01:07:51 +0100

    task 0. Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ task 0. Async Generator """
-import random
+from typing import Generator
 import asyncio
+import random
 
 
-async def async_generator():
-    """  asynchronous coroutine  """
+async def async_generator() -> Generator[float, None, None]:
+    """  asynchronous generator  """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
