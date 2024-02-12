25a11c30807bfcb85a226b8e8a8d22a18854fbd2
mxnc <thibaultmaxence44@gmail.com>
2024-02-12 09:37:29 +0100

    Task 1. FIFO caching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Task 1. FIFO caching """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ inherits from BaseCaching and is a caching system """
+    def __init__(self):
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """ assign to the dictionary """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
+    
+    def get(self, key):
+        """ return the value linked to key"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
