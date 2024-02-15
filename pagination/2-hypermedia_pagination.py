dab8bc9a6f391813cb5c02cf6cdc86237ccf8da8
mxnc <thibaultmaxence44@gmail.com>
2024-02-15 13:45:53 +0100

    task 2. Hypermedia pagination

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+task 2. Hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """ Initialize instance. """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        ''' Return page of dataset. '''
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        indices = index_range(page, page_size)
+        start = indices[0]
+        end = indices[1]
+
+        try:
+            return self.dataset()[start:end]
+        except IndexError:
+            return []
+        
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        returns a dictionary containing the following key-value pairs
+
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
+        """
+        page_data = self.get_page(page, page_size)
+        total_data = len(self.dataset())
+        total_pages = math.ceil(total_data / page_size)
+
+        return {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page != 1 else None,
+            'total_pages': total_pages
+        }
