c8600859e1f31f84edc987536dcab6f19d3b3038
mxnc <thibaultmaxence44@gmail.com>
2024-02-20 12:29:09 +0100

    fix pycodstyle

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,6 +7,7 @@ def hash_password(password: str) -> bytes:
     """ encrypting passwords """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ check valid password """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
