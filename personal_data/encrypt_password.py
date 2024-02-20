ba7387f1e121b739f69181b5a5c5b3572cc9692b
mxnc <thibaultmaxence44@gmail.com>
2024-02-20 12:27:51 +0100

    hash password

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" hash password """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ encrypting passwords """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check valid password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
