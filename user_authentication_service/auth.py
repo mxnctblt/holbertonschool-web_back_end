70f60f5e0bc3a3ce09336b4f8a271fe6f03e01dd
mxnc <thibaultmaxence44@gmail.com>
2024-02-29 12:18:58 +0100

    Task 18. Update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -79,3 +79,16 @@ class Auth:
                 return reset_token
             except NoResultFound:
                 raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update password """
+        if reset_token and password:
+            try:
+                user = self._db.find_user_by(reset_token=reset_token)
+            except NoResultFound:
+                raise ValueError
+            else:
+                hashed_password = _hash_password(password)
+                self._db.update_user(user.id, hashed_password=hashed_password,
+                                     reset_token=None)
+                return None
