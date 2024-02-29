d7f479f2f32eb546991b7577773089460cf949d0
mxnc <thibaultmaxence44@gmail.com>
2024-02-29 12:31:37 +0100

    Task 19. Update password end-point

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -79,5 +79,18 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ update password """
+    email = request.form.get('email')
+    rest_token = request.form.get('rest_token')
+    password = request.form.get('new_password')
+    try:
+        AUTH.update_password(rest_token, password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
