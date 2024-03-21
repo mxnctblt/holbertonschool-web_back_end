5eeafa3bf73f8c30f7c6a9267c058ae12d79e654
mxnc <thibaultmaxence44@gmail.com>
2024-03-21 23:22:23 +0100

    fixing

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,21 +2,18 @@
 """ Redis Module """
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Optional
 from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
     """ def count calls """
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
+    def wrapper(self, *args, **kwds):
         """ def wrapper """
-        # Generate a key using the qualified name of the method
-        key = method.__qualname__
-        # Increment the count for the method
-        self._redis.incr(key)
-        # Call the original method and return its result
-        return method(self, *args, **kwargs)
+        key_m = method.__qualname__
+        self._redis.incr(key_m)
+        return method(self, *args, **kwds)
     return wrapper
 
 
@@ -36,14 +33,17 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """ def get """
         data = self._redis.get(key)
         return data if not fn else fn(data)
 
     def get_str(self, key: str):
+        """ def get str """
         data = self._redis.get(key)
         return data.decode("utf-8")
 
     def get_int(self, key: str):
+        """ def get int """
         return self.get(key, int)
