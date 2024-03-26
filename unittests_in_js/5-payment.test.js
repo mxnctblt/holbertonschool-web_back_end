c002842e584d04f3e874c145cfc964f940c87c9b
mxnc <thibaultmaxence44@gmail.com>
2024-03-26 10:26:53 +0100

    adding beforeEach and afterEach

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -3,21 +3,24 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', function() {
-    it('Checks 100 + 20', function() {
-        const log = sinon.spy(console, 'log');
+    let log;
 
+    beforeEach(function() {
+        log = sinon.spy(console, 'log');
+    });
+    afterEach(function() {
+        log.restore();
+    });
+
+    it('Checks 100 + 20', function() {
         sendPaymentRequestToApi(100, 20);
 
-        log.restore();
         sinon.assert.calledOnce(log);
         sinon.assert.calledWith(log, 'The total is: 120');
     });
     it('Checks 10 + 10', function() {
-        const log = sinon.spy(console, 'log');
-
         sendPaymentRequestToApi(10, 10);
 
-        log.restore();
         sinon.assert.calledOnce(log);
         sinon.assert.calledWith(log, 'The total is: 20');
     });
