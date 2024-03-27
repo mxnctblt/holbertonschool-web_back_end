7927dbb7ec49a3fefcc266099c147c1dc927eb11
mxnc <thibaultmaxence44@gmail.com>
2024-03-27 10:49:39 +0100

    menger

---
+++ queuing_system_in_js/0-menger.c
@@ -0,0 +1,37 @@
+#include "menger.h"
+
+/**
+ * menger - Draws a 2D Menger Sponge
+ * @level: the level of the Menger Sponge to draw
+ * Return: Nothing
+ */
+void menger(int level)
+{
+    int size = pow(3, level), i, j, quotient, print_sharp;
+
+    if (level < 0)
+        return;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            quotient = size / 3;
+            print_sharp = 1;
+            while (quotient >= 1)
+            {
+                if ((i / quotient) % 3 == 1 &&
+                    (j / quotient) % 3 == 1)
+                {
+                    printf(" ");
+                    print_sharp = 0;
+                    break;
+                }
+                quotient /= 3;
+            }
+            if (print_sharp == 1)
+                printf("#");
+        }
+        printf("\n");
+    }
+}
